0b063c9327125523b8777306ce2ca6dfe417389a
Kevin Li <ychiaoli18@gmail.com>
2016-06-09 23:20:08 +0800

    api/account: Add the interface of account api and error handling

---
+++ bb8/error.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+    bb8 application error
+    ~~~~~~~~~~~~~~~
+
+    Copyright 2016 bb8 Authors
+"""
+
+class AppError(Exception):
+    """ Custom application error """
+
+    def __init__(self, status_code, error_code, message):
+        if status_code >= 600 or status_code < 200:
+            status_code = 500
+        self.status_code = status_code
+        self.error_code = error_code
+        self.message = message
+
+    def __str__(self):
+        return "status_code: %d, error_code: %d, message: %s" % (self.status_code, self.error_code, self.message)
