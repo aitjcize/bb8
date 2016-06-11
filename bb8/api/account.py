951888fb01886055da1c12e3d942c75bb799bfc8
Kevin Li <kevin@compose.ai>
2016-06-11 11:04:12 +0800

    api/account: Finish email_register

--- bb8/api/account.py
+++ bb8/api/account.py
@@ -6,18 +6,40 @@
     Copyright 2016 bb8 Authors
 """
 
-from flask import jsonify
+from flask import jsonify, session
 
 from bb8 import app, AppError
-from bb8.constant import HTTPStatus, CustomError
+from bb8.constant import HTTPStatus, CustomError, Key
 from bb8.api.forms import RegistrationForm, SocialRegistrationForm, LoginForm
+from bb8.api.middlewares import login_required
+from bb8.backend.database import Account
 
 
 @app.route('/email_register', methods=['POST'])
 def email_register():
     form = RegistrationForm(csrf_enabled=False)
     if form.validate_on_submit():
-        return jsonify(message='ok')
+        user_info = {
+            'username': form.data['username'],
+            'email': form.data['email'],
+        }
+        account = Account.get_by(single=True, **user_info)
+        if not account:
+            account = Account(**user_info) \
+                        .set_passwd(form.data['passwd']) \
+                        .add()
+            Account.commit()
+        else:
+            raise AppError(
+                HTTPStatus.STATUS_CLIENT_ERROR,
+                CustomError.ERR_USER_EXISTED,
+                "username {username} or email {email} is already taken"
+                .format(**user_info))
+
+        session[Key.ACCESS_TOKEN] = account.auth_token
+
+        # TODO: Send a email confirmation
+        return jsonify(account.to_json())
     raise AppError(HTTPStatus.STATUS_CLIENT_ERROR,
                    CustomError.ERR_FORM_VALIDATION, form.errors)
 
@@ -35,6 +57,8 @@ def social_register():
 def login():
     form = LoginForm(csrf_enabled=False)
     if form.validate_on_submit():
+        account = Account.get_by(id=1, single=True)
+        session[Key.ACCESS_TOKEN] = account.auth_token
         return jsonify(message='ok')
     raise AppError(HTTPStatus.STATUS_CLIENT_ERROR,
                    CustomError.ERR_FORM_VALIDATION, form.errors)
@@ -51,5 +75,6 @@ def verify_email():
 
 
 @app.route('/me', methods=['GET'])
+@login_required
 def get_me():
     return jsonify(message='ok')
---
+++ bb8/api/middlewares.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+    middlewares
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    Copyright 2016 bb8 Authors
+"""
+
+from functools import wraps
+
+from flask import g, session
+
+from bb8.error import AppError
+from bb8.constant import HTTPStatus, CustomError, Key
+from bb8.backend.database import Account
+
+
+def login_required(func):
+    @wraps(func)
+    def decorated(*args, **kwargs):
+        if Key.ACCESS_TOKEN in session:
+            token = session[Key.ACCESS_TOKEN]
+            g.account = Account.from_auth_token(token)
+            return func(*args, **kwargs)
+        raise AppError(HTTPStatus.STATUS_CLIENT_ERROR,
+                       CustomError.ERR_UNAUTHENTICATED,
+                       'Not loggined')
+    return decorated
