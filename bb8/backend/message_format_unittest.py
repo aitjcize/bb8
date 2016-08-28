0567f14f1f005fa2280b8f923528c27462b6c28e
Wei-Ning Huang <aitjcize@compose.ai>
2016-08-28 07:15:39 +0000

    messaging: move Message definition to message_format module

---
+++ bb8/backend/message_format_unittest.py
@@ -0,0 +1,235 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+    Messaging unittest
+    ~~~~~~~~~~~~~~~~~~
+
+    Copyright 2016 bb8 Authors
+"""
+
+import datetime
+import time
+import unittest
+
+import jsonschema
+import pytz
+
+from flask import g
+
+from bb8 import app
+from bb8.backend.database import (Bot, ColletedDatum, DatabaseManager,
+                                  Platform, PlatformTypeEnum, User)
+from bb8.backend.message_format import Message
+
+
+class MessageUnittest(unittest.TestCase):
+    def setUp(self):
+        self.dbm = DatabaseManager()
+        self.dbm.connect()
+        self.setup_prerequisite()
+
+    def tearDown(self):
+        self.dbm.disconnect()
+
+    def setup_prerequisite(self):
+        self.dbm.reset()
+
+        self.bot = Bot(name=u'test', description=u'test',
+                       interaction_timeout=120, session_timeout=86400).add()
+        self.dbm.commit()
+
+        self.platform = Platform(bot_id=self.bot.id,
+                                 type_enum=PlatformTypeEnum.Facebook,
+                                 provider_ident='facebook_page_id',
+                                 config={}).add()
+        self.dbm.commit()
+
+        self.user_1 = User(bot_id=self.bot.id,
+                           platform_id=self.platform.id,
+                           platform_user_ident='1153206858057166',
+                           last_seen=datetime.datetime(2016, 6, 2, 12, 44, 56,
+                                                       tzinfo=pytz.utc)).add()
+        self.user_2 = User(bot_id=self.bot.id,
+                           platform_id=self.platform.id,
+                           platform_user_ident='1318395614844436',
+                           last_seen=datetime.datetime(2016, 6, 2, 12, 44, 56,
+                                                       tzinfo=pytz.utc)).add()
+        self.dbm.commit()
+
+    def test_Button(self):
+        with self.assertRaises(RuntimeError):
+            Message.Button('wrong_type', 'test', 'http://test.com')
+
+        with self.assertRaises(jsonschema.exceptions.ValidationError):
+            b = Message.Button(Message.ButtonType.WEB_URL, 'test',
+                               payload='payload')
+            jsonschema.validate(b.as_dict(), Message.Button.schema())
+
+        b = Message.Button(Message.ButtonType.WEB_URL, 'test',
+                           url='http://test.com')
+        jsonschema.validate(b.as_dict(), Message.Button.schema())
+        self.assertEquals(b, b.FromDict(b.as_dict()))
+
+        b = Message.Button(Message.ButtonType.POSTBACK, 'postback',
+                           payload='postback')
+        jsonschema.validate(b.as_dict(), Message.Button.schema())
+        self.assertEquals(b, b.FromDict(b.as_dict()))
+
+    def test_Bubble(self):
+        b = Message.Bubble('title')
+        jsonschema.validate(b.as_dict(), Message.Bubble.schema())
+        self.assertEquals(b, b.FromDict(b.as_dict()))
+
+        b = Message.Bubble('title', 'http://test.com/item_url',
+                           'http://test.com/image_url', 'subtitle')
+        jsonschema.validate(b.as_dict(), Message.Bubble.schema())
+        self.assertEquals(b, b.FromDict(b.as_dict()))
+
+        b.add_button(Message.Button(Message.ButtonType.WEB_URL, 'test',
+                                    url='http://test.com'))
+        b.add_button(Message.Button(Message.ButtonType.POSTBACK, 'test',
+                                    payload='payload'))
+        jsonschema.validate(b.as_dict(), Message.Bubble.schema())
+        self.assertEquals(b, b.FromDict(b.as_dict()))
+
+    def test_QuickReply(self):
+        q1 = Message.QuickReply('quick_reply_1',
+                                acceptable_inputs=['1', '2'])
+        jsonschema.validate(q1.as_dict(), Message.QuickReply.schema())
+        self.assertEquals(q1, q1.FromDict(q1.as_dict()))
+
+        q2 = Message.QuickReply('quick_reply_2',
+                                acceptable_inputs=['3', '4'])
+
+        m = Message('test')
+        m.add_quick_reply(q1)
+        m.add_quick_reply(q2)
+
+        jsonschema.validate(m.as_dict(), Message.schema())
+        self.assertEquals(m, m.FromDict(m.as_dict()))
+
+        transform_keys = reduce(lambda x, y: x + y,
+                                [x[0] for x in g.input_transformation], [])
+        self.assertTrue('quick_reply_1' in transform_keys)
+        self.assertTrue('1' in transform_keys)
+        self.assertTrue('3' in transform_keys)
+
+    def test_Message(self):
+        but1 = Message.Button(Message.ButtonType.WEB_URL, 'test',
+                              url='http://test.com')
+        but2 = Message.Button(Message.ButtonType.POSTBACK, 'test',
+                              payload='payload')
+        b = Message.Bubble('title', 'http://test.com/item_url',
+                           'http://test.com/image_url', 'subtitle')
+        b.add_button(but1)
+        b.add_button(but2)
+
+        m = Message('test')
+        jsonschema.validate(m.as_dict(), Message.schema())
+        self.assertEquals(m, m.FromDict(m.as_dict()))
+
+        m = Message(image_url='http://test.com/image_url')
+        jsonschema.validate(m.as_dict(), Message.schema())
+        self.assertEquals(m, m.FromDict(m.as_dict()))
+
+        # Button message
+        m = Message()
+        m.set_buttons_text('question')
+        m.add_button(but1)
+        m.add_button(but2)
+        jsonschema.validate(m.as_dict(), Message.schema())
+        self.assertEquals(m, m.FromDict(m.as_dict()))
+
+        transform_keys = reduce(lambda x, y: x + y,
+                                [x[0] for x in g.input_transformation], [])
+        self.assertTrue('^1$' not in transform_keys)
+        self.assertTrue('^2$' in transform_keys)
+
+        # Generic message
+        g.input_transformation = []
+        m = Message()
+        m.add_bubble(b)
+        m.add_bubble(b)
+        jsonschema.validate(m.as_dict(), Message.schema())
+        self.assertEquals(m, m.FromDict(m.as_dict()))
+
+        transform_keys = reduce(lambda x, y: x + y,
+                                [x[0] for x in g.input_transformation], [])
+        self.assertTrue('^1-2$' in transform_keys)
+        self.assertTrue('^2-2$' in transform_keys)
+
+        with self.assertRaises(RuntimeError):
+            m = Message('test', 'url')
+
+    def test_message_variable_rendering(self):
+        """Test that variable in message can be rendered correctly."""
+        variables = {
+            'user': {
+                'first_name': 'Isaac',
+                'last_name': 'Huang',
+            },
+            'date': 'today'
+        }
+        m = Message('Hi {{user.first_name}}', variables=variables)
+        self.assertEquals(m.as_dict()['text'], 'Hi Isaac')
+
+        m = Message()
+        bubble = Message.Bubble('Bubble Test',
+                                'http://www.starwars.com/',
+                                'http://i.imgur.com/4loi6PJ.jpg',
+                                '{{user.first_name}}', variables=variables)
+        bubble.add_button(Message.Button(Message.ButtonType.WEB_URL,
+                                         '{{user.last_name}}',
+                                         url='http://www.starwars.com/',
+                                         variables=variables))
+        m.add_bubble(bubble)
+        bubble = m.as_dict()['attachment']['payload']['elements'][0]
+
+        self.assertEquals(bubble['subtitle'], 'Isaac')
+        self.assertEquals(bubble['buttons'][0]['title'], 'Huang')
+
+    def test_query_expression_rendering(self):
+        """Test that query expresssion can be query and rendered correctly."""
+        ColletedDatum(user_id=self.user_1.id, key='data', value='value1').add()
+        DatabaseManager.commit()
+        time.sleep(1)
+        ColletedDatum(user_id=self.user_1.id, key='data', value='value2').add()
+        DatabaseManager.commit()
+        time.sleep(1)
+        ColletedDatum(user_id=self.user_1.id, key='data', value='value3').add()
+        ColletedDatum(user_id=self.user_1.id, key='aaa', value='aaa').add()
+        ColletedDatum(user_id=self.user_2.id, key='data', value='value4').add()
+        DatabaseManager.commit()
+
+        g.user = self.user_1
+        m = Message('{{q.data|first|upper}}')
+        self.assertEquals(m.as_dict()['text'], 'VALUE1')
+
+        m = Message("{{q.data|get(1)}}")
+        self.assertEquals(m.as_dict()['text'], 'value2')
+
+        m = Message("{{q.data|last}}")
+        self.assertEquals(m.as_dict()['text'], 'value3')
+
+        m = Message("{{q.data|lru(0)}}")
+        self.assertEquals(m.as_dict()['text'], 'value3')
+
+        m = Message("{{q.data|lru(1)}}")
+        self.assertEquals(m.as_dict()['text'], 'value2')
+
+        m = Message("{{q.data|get(5)|fallback('valuef')}}")
+        self.assertEquals(m.as_dict()['text'], 'valuef')
+
+        m = Message("{{q.data|order_by('-created_at')|first}}")
+        self.assertEquals(m.as_dict()['text'], 'value3')
+
+        m = Message("{{q.data|count}}")
+        self.assertEquals(m.as_dict()['text'], '3')
+
+        m = Message("{{qall.data|count}}")
+        self.assertEquals(m.as_dict()['text'], '4')
+
+
+if __name__ == '__main__':
+    with app.test_request_context():
+        unittest.main()
--- bb8/backend/messaging_unittest.py
+++ bb8/backend/messaging_unittest.py
@@ -8,126 +8,14 @@
 """
 
 import datetime
-import time
 import unittest
 
-import jsonschema
 import pytz
 
-from flask import g
-
-from bb8 import app
-from bb8.backend.database import (Bot, ColletedDatum, DatabaseManager,
-                                  Platform, PlatformTypeEnum, User)
-from bb8.backend.messaging import Message, broadcast_message
-from bb8.backend.messaging_provider import facebook
-
-
-class MessageUnittest(unittest.TestCase):
-    def test_Button(self):
-        with self.assertRaises(RuntimeError):
-            Message.Button('wrong_type', 'test', 'http://test.com')
-
-        with self.assertRaises(jsonschema.exceptions.ValidationError):
-            b = Message.Button(Message.ButtonType.WEB_URL, 'test',
-                               payload='payload')
-            jsonschema.validate(b.as_dict(), Message.Button.schema())
-
-        b = Message.Button(Message.ButtonType.WEB_URL, 'test',
-                           url='http://test.com')
-        jsonschema.validate(b.as_dict(), Message.Button.schema())
-        self.assertEquals(b, b.FromDict(b.as_dict()))
-
-        b = Message.Button(Message.ButtonType.POSTBACK, 'postback',
-                           payload='postback')
-        jsonschema.validate(b.as_dict(), Message.Button.schema())
-        self.assertEquals(b, b.FromDict(b.as_dict()))
-
-    def test_Bubble(self):
-        b = Message.Bubble('title')
-        jsonschema.validate(b.as_dict(), Message.Bubble.schema())
-        self.assertEquals(b, b.FromDict(b.as_dict()))
-
-        b = Message.Bubble('title', 'http://test.com/item_url',
-                           'http://test.com/image_url', 'subtitle')
-        jsonschema.validate(b.as_dict(), Message.Bubble.schema())
-        self.assertEquals(b, b.FromDict(b.as_dict()))
-
-        b.add_button(Message.Button(Message.ButtonType.WEB_URL, 'test',
-                                    url='http://test.com'))
-        b.add_button(Message.Button(Message.ButtonType.POSTBACK, 'test',
-                                    payload='payload'))
-        jsonschema.validate(b.as_dict(), Message.Bubble.schema())
-        self.assertEquals(b, b.FromDict(b.as_dict()))
-
-    def test_QuickReply(self):
-        q1 = Message.QuickReply('quick_reply_1',
-                                acceptable_inputs=['1', '2'])
-        jsonschema.validate(q1.as_dict(), Message.QuickReply.schema())
-        self.assertEquals(q1, q1.FromDict(q1.as_dict()))
-
-        q2 = Message.QuickReply('quick_reply_2',
-                                acceptable_inputs=['3', '4'])
-
-        m = Message('test')
-        m.add_quick_reply(q1)
-        m.add_quick_reply(q2)
-
-        jsonschema.validate(m.as_dict(), Message.schema())
-        self.assertEquals(m, m.FromDict(m.as_dict()))
-
-        transform_keys = reduce(lambda x, y: x + y,
-                                [x[0] for x in g.input_transformation], [])
-        self.assertTrue('quick_reply_1' in transform_keys)
-        self.assertTrue('1' in transform_keys)
-        self.assertTrue('3' in transform_keys)
-
-    def test_Message(self):
-        but1 = Message.Button(Message.ButtonType.WEB_URL, 'test',
-                              url='http://test.com')
-        but2 = Message.Button(Message.ButtonType.POSTBACK, 'test',
-                              payload='payload')
-        b = Message.Bubble('title', 'http://test.com/item_url',
-                           'http://test.com/image_url', 'subtitle')
-        b.add_button(but1)
-        b.add_button(but2)
-
-        m = Message('test')
-        jsonschema.validate(m.as_dict(), Message.schema())
-        self.assertEquals(m, m.FromDict(m.as_dict()))
-
-        m = Message(image_url='http://test.com/image_url')
-        jsonschema.validate(m.as_dict(), Message.schema())
-        self.assertEquals(m, m.FromDict(m.as_dict()))
-
-        # Button message
-        m = Message()
-        m.set_buttons_text('question')
-        m.add_button(but1)
-        m.add_button(but2)
-        jsonschema.validate(m.as_dict(), Message.schema())
-        self.assertEquals(m, m.FromDict(m.as_dict()))
-
-        transform_keys = reduce(lambda x, y: x + y,
-                                [x[0] for x in g.input_transformation], [])
-        self.assertTrue('^1$' not in transform_keys)
-        self.assertTrue('^2$' in transform_keys)
-
-        # Generic message
-        g.input_transformation = []
-        m = Message()
-        m.add_bubble(b)
-        m.add_bubble(b)
-        jsonschema.validate(m.as_dict(), Message.schema())
-        self.assertEquals(m, m.FromDict(m.as_dict()))
-
-        transform_keys = reduce(lambda x, y: x + y,
-                                [x[0] for x in g.input_transformation], [])
-        self.assertTrue('^1-2$' in transform_keys)
-        self.assertTrue('^2-2$' in transform_keys)
-
-        with self.assertRaises(RuntimeError):
-            m = Message('test', 'url')
+from bb8.backend.database import (Bot, DatabaseManager, Platform,
+                                  PlatformTypeEnum, User)
+from bb8.backend import messaging
+from bb8.backend.message_format import Message
 
 
 class MessagingUnittest(unittest.TestCase):
@@ -167,23 +55,7 @@ class MessagingUnittest(unittest.TestCase):
     def test_broadcast_message(self):
         """Test facebook message sending."""
         # Test card message
-        m = Message()
-        bubble = Message.Bubble('Bubble Test',
-                                'http://www.starwars.com/',
-                                'http://i.imgur.com/4loi6PJ.jpg',
-                                'Bubble subtitle')
-        bubble.add_button(Message.Button(Message.ButtonType.WEB_URL,
-                                         'Starwars',
-                                         url='http://www.starwars.com/'))
-        bubble.add_button(Message.Button(Message.ButtonType.WEB_URL,
-                                         'Google',
-                                         url='http://www.google.com/'))
-        bubble.add_button(Message.Button(Message.ButtonType.WEB_URL,
-                                         '17',
-                                         url='http://www.17.media/'))
-        m.add_bubble(bubble)
-        m.add_bubble(bubble)
-        m.add_bubble(bubble)
+        m = Message('Message test')
 
         context = {}
         context['count'] = 0
@@ -191,80 +63,11 @@ class MessagingUnittest(unittest.TestCase):
         def send_message_mock(unused_user, unused_messages):
             context['count'] += 1
 
-        facebook.send_message = send_message_mock
-        broadcast_message(self.bot, [m, m])
+        messaging.send_message = send_message_mock
+        messaging.broadcast_message(self.bot, [m, m])
 
         self.assertEquals(context['count'], 2)
 
-    def test_message_variable_rendering(self):
-        """Test that variable in message can be rendered correctly."""
-        variables = {
-            'user': {
-                'first_name': 'Isaac',
-                'last_name': 'Huang',
-            },
-            'date': 'today'
-        }
-        m = Message('Hi {{user.first_name}}', variables=variables)
-        self.assertEquals(m.as_dict()['text'], 'Hi Isaac')
-
-        m = Message()
-        bubble = Message.Bubble('Bubble Test',
-                                'http://www.starwars.com/',
-                                'http://i.imgur.com/4loi6PJ.jpg',
-                                '{{user.first_name}}', variables=variables)
-        bubble.add_button(Message.Button(Message.ButtonType.WEB_URL,
-                                         '{{user.last_name}}',
-                                         url='http://www.starwars.com/',
-                                         variables=variables))
-        m.add_bubble(bubble)
-        bubble = m.as_dict()['attachment']['payload']['elements'][0]
-
-        self.assertEquals(bubble['subtitle'], 'Isaac')
-        self.assertEquals(bubble['buttons'][0]['title'], 'Huang')
-
-    def test_query_expression_rendering(self):
-        """Test that query expresssion can be query and rendered correctly."""
-        ColletedDatum(user_id=self.user_1.id, key='data', value='value1').add()
-        DatabaseManager.commit()
-        time.sleep(1)
-        ColletedDatum(user_id=self.user_1.id, key='data', value='value2').add()
-        DatabaseManager.commit()
-        time.sleep(1)
-        ColletedDatum(user_id=self.user_1.id, key='data', value='value3').add()
-        ColletedDatum(user_id=self.user_1.id, key='aaa', value='aaa').add()
-        ColletedDatum(user_id=self.user_2.id, key='data', value='value4').add()
-        DatabaseManager.commit()
-
-        g.user = self.user_1
-        m = Message('{{q.data|first|upper}}')
-        self.assertEquals(m.as_dict()['text'], 'VALUE1')
-
-        m = Message("{{q.data|get(1)}}")
-        self.assertEquals(m.as_dict()['text'], 'value2')
-
-        m = Message("{{q.data|last}}")
-        self.assertEquals(m.as_dict()['text'], 'value3')
-
-        m = Message("{{q.data|lru(0)}}")
-        self.assertEquals(m.as_dict()['text'], 'value3')
-
-        m = Message("{{q.data|lru(1)}}")
-        self.assertEquals(m.as_dict()['text'], 'value2')
-
-        m = Message("{{q.data|get(5)|fallback('valuef')}}")
-        self.assertEquals(m.as_dict()['text'], 'valuef')
-
-        m = Message("{{q.data|order_by('-created_at')|first}}")
-        self.assertEquals(m.as_dict()['text'], 'value3')
-
-        m = Message("{{q.data|count}}")
-        self.assertEquals(m.as_dict()['text'], '3')
-
-        m = Message("{{qall.data|count}}")
-        self.assertEquals(m.as_dict()['text'], '4')
-
 
 if __name__ == '__main__':
-    with app.test_request_context():
-        unittest.main()
+    unittest.main()
