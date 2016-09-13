b5bf828713adf0ff77222124557ba02146b728cb
Wei-Ning Huang <aitjcize@compose.ai>
2016-09-13 17:52:40 +0000

    content_modules: add noop module

---
+++ bb8/backend/content_modules/noop.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+    No operation
+    ~~~~~~~~~~~~
+
+    Copyright 2016 bb8 Authors
+"""
+
+from bb8.backend.module_api import SupportedPlatform
+
+
+def get_module_info():
+    return {
+        'id': 'ai.compose.core.noop',
+        'name': 'No operation',
+        'description': 'Do nothing',
+        'supported_platform': SupportedPlatform.All,
+        'module_name': 'noop',
+        'ui_module_name': 'noop',
+    }
+
+
+def schema():
+    return {
+        'type': 'object'
+    }
+
+
+def run(unused_content_config, unused_env, unused_variables):
+    return []
