64c23590462d8da6f8812afbb05617ae576e49ce
Wei-Ning Huang <aitjcize@compose.ai>
2016-09-17 19:12:26 +0000

    hotfix: fix alembic migration script

--- alembic/versions/248e43bdefd0_add_user_memory_settings_field.py
+++ alembic/versions/248e43bdefd0_add_user_memory_settings_field.py
@@ -12,13 +12,25 @@ down_revision = 'b08fa78fbb27'
 branch_labels = None
 depends_on = None
 
-from alembic import op
+import pickle
+
 import sqlalchemy as sa
+
+from alembic import op
+from sqlalchemy.sql import table, column
 
 
 def upgrade():
-    op.add_column('user', sa.Column('memory', sa.PickleType(), nullable=False))
-    op.add_column('user', sa.Column('settings', sa.PickleType(), nullable=False))
+    op.add_column('user', sa.Column('memory', sa.PickleType(), nullable=True))
+    op.add_column('user', sa.Column('settings', sa.PickleType(), nullable=True))
+    user = table('user', column('memory'), column('settings'))
+    try:
+        op.execute(user.update().values(memory=pickle.dumps({})))
+        op.execute(user.update().values(settings=pickle.dumps({})))
+    except: pass
+    op.alter_column('user', 'memory', nullable=False)
+    op.alter_column('user', 'settings', nullable=False)
+
 
 
 def downgrade():
