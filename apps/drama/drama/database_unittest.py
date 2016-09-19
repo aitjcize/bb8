cc42b710ef3ce654d1e984427647253f0aca3795
Wei-Ning Huang <aitjcize@compose.ai>
2016-09-19 16:02:22 +0000

    apps: drama: fix unitest warning

--- apps/drama/drama/database_unittest.py
+++ apps/drama/drama/database_unittest.py
@@ -29,32 +29,32 @@ class SchemaUnittest(unittest.TestCase):
         """Populate data into all tables and make sure there are no error."""
         DatabaseManager.reset()
 
-        drama = Drama(link='mock-link',
-                      name='mock-name',
-                      description='mock-desc',
-                      image='mock-image',
+        drama = Drama(link=u'mock-link',
+                      name=u'mock-name',
+                      description=u'mock-desc',
+                      image=u'mock-image',
                       country=DramaCountryEnum.TAIWAN,
                       order=1).add()
 
         DatabaseManager.commit()
 
-        drama1 = Drama.get_by(name='mock-name', single=True)
+        drama1 = Drama.get_by(name=u'mock-name', single=True)
         self.assertNotEquals(drama1, None)
         self.assertEquals(drama1.link, 'mock-link')
 
-        episode = Episode(link='mock-link-2',
+        episode = Episode(link=u'mock-link-2',
                           drama_id=drama.id,
                           serial_number=10).add()
 
         drama.episodes.append(episode)
         DatabaseManager.commit()
 
-        episode1 = Episode.get_by(link='mock-link-2', single=True)
+        episode1 = Episode.get_by(link=u'mock-link-2', single=True)
         self.assertNotEquals(episode1, None)
 
         self.assertEquals(episode1.drama.id, drama1.id)
 
-        drama2 = Drama.get_by(name='mock-name', single=True)
+        drama2 = Drama.get_by(name=u'mock-name', single=True)
         self.assertEquals(len(drama2.episodes), 1)
         self.assertEquals(drama2.episodes[0].link_hash, episode.link_hash)
 
