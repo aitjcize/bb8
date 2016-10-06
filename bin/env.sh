c5256926030634b8f4a9fc83e5ab4cb97a989ca7
Yung-Chieh Lo <yjlou.tw@gmail.com>
2016-10-06 05:26:35 +0000

    env.sh: add yjlou's setting

--- bin/env.sh
+++ bin/env.sh
@@ -40,6 +40,11 @@ case $USER in
     export REDIS_PORT=6381
     export HTTP_PORT=7002
     ;;
+  yjlou)
+    export MYSQL_PORT=3310
+    export REDIS_PORT=6382
+    export HTTP_PORT=7003
+    ;;
 esac
 
 export DATABASE="mysql+pymysql://bb8:bb8test@127.0.0.1:$MYSQL_PORT/bb8?charset=utf8mb4"
