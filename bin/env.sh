00f2196e0bb4890b2552447c1783b284b0dc1a41
Amy Tseng <aammyytseng@gmail.com>
2016-10-13 16:00:36 +0000

    env.sh: add amytseng user config

--- bin/env.sh
+++ bin/env.sh
@@ -58,6 +58,13 @@ case $USER in
     export APP_RPC_PORT=10002
     export WEBPACK_PORT=8083
     ;;
+  amytseng)
+    export MYSQL_PORT=3312
+    export REDIS_PORT=6384
+    export HTTP_PORT=7005
+    export APP_RPC_PORT=10003
+    export WEBPACK_PORT=8084
+    ;;
 esac
 
 
