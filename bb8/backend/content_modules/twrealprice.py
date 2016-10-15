36be29af1ba749fb01dd1b2db9c81a0337d79855
Yung-Chieh Lo <yjlou.tw@gmail.com>
2016-10-15 08:21:02 +0000

    twrealprice: v2. improve geocoding quality: universal search.

--- bb8/backend/content_modules/twrealprice.py
+++ bb8/backend/content_modules/twrealprice.py
@@ -4,6 +4,9 @@
     ~~~~~~~~~~~~~~~~~~~~~~~
 
     Provide Taiwan eState info.
+
+    Note that all transaction data are encoded in utf-8 (specifically trans
+    and cached_transaction). All other string in this file should be unicode.
 """
 
 import cPickle
@@ -14,8 +17,9 @@ import time
 import traceback
 import urllib
 
-from bb8.backend.module_api import (Config, Message, Memory, Resolve,
-                                    SupportedPlatform)
+from bb8.backend.module_api import (Config, Message, Memory,
+                                    SupportedPlatform, LocationPayload)
+from bb8.backend.content_modules.geocoding import GoogleMapsGeocodingAPI
 import twrealprice_rule
 
 
@@ -26,7 +30,7 @@ _LOG.setLevel(logging.INFO)
 GOOGLE_STATIC_MAP_API_KEY = 'AIzaSyCpP0pV9_YtQuV-Gzf6aL9nRt1Es5UlBv4'
 
 # Maximum number of transaction to cache for "more data" feature.
-MAX_CACHE_TRANS = 49  # 7 pages
+MAX_CACHE_TRANS = 50  # 50 / 5 = 10 pages
 
 # For lat/lng
 GRID_NUM = 1000
@@ -78,10 +82,12 @@ def GenNearbyIndex(lat_center, lng_center, N):
 
 
 def RocSlash(roc_date):
+    """Returns utf-8"""
     return '%s/%s/%s' % (roc_date[0:3], roc_date[3:5], roc_date[5:7])
 
 
 def Age(build_date):
+    """Returns utf-8"""
     try:
         return '%.f' % ((
             datetime.date.today() -
@@ -91,10 +97,33 @@ def Age(build_date):
 
 
 def StreetOnly(s):
+    """Returns utf-8"""
     sub = s['土地區段位置或建物區門牌'].split(s['鄉鎮市區'])
     if len(sub) == 2:
         return sub[1]
     return s['土地區段位置或建物區門牌']
+
+
+def MarkerStyle():
+    yield 'color:purple'
+    i = -1
+    while True:
+        i += 1
+        yield 'color:red|label:%c' % (i + ord('A'))
+
+
+def Deser(obj):
+    """Helper function for de-serialization.
+
+    Args:
+      obj: None or pickled str.
+
+    Returns:
+      None or the object.
+    """
+    if obj:
+        return cPickle.loads(obj)
+    return None
 
 
 def get_module_info():
@@ -111,35 +140,7 @@ def get_module_info():
 def schema():
     return {
         'type': 'object',
-        'required': ['api_key', 'max_count', 'location', 'distance_threshold'],
         'properties': {
-            'api_key': {
-                'type': 'string',
-            },
-            'max_count': {
-                'type': 'integer',
-                'minimum': 1,
-                'maximum': 100
-            },
-            'location': {
-                'oneOf': [{
-                    'type': 'string'
-                }, {
-                    'type': 'object',
-                    'required': ['coordinates'],
-                    'properties': {
-                        'coordinates': {
-                            'type': 'object',
-                            'required': ['lat', 'long'],
-                            'properties': {
-                                'lat': {'type': 'number'},
-                                'long': {'type': 'number'}
-                            }
-                        }
-                    }
-                }]
-            },
-            'distance_threshold': {'type': 'number'},
         }
     }
 
@@ -155,8 +156,8 @@ class GoogleStaticMapAPIRequestBuilder(object):
         self._maptype = 'roadmap'
         self._markers = {}
 
-    def add_marker(self, coordinate, color='red'):
-        self._markers[coordinate] = color
+    def add_marker(self, coordinate, style):
+        self._markers[coordinate] = style
 
     def remove_marker(self, coordinate):
         del self._markers[coordinate]
@@ -165,9 +166,9 @@ class GoogleStaticMapAPIRequestBuilder(object):
         self._markers = {}
 
     def _markers_string(self):
-        return '&'.join(['markers=color:%s|%3.8f,%3.8f' %
-                         ((color,) + coordinate)
-                         for coordinate, color in self._markers.iteritems()])
+        return '&'.join(['markers=%s|%3.8f,%3.8f' %
+                         ((style,) + coordinate)
+                         for coordinate, style in self._markers.iteritems()])
 
     def build_url(self):
         url = ('%s?size=%s&maptype=%s&' %
@@ -184,7 +185,17 @@ class TwRealPrice(object):
     def __init__(self):
         super(TwRealPrice, self).__init__()
 
-    def nearby_transaction(self, max_count, coordinate, unused_threshold=0):
+    def nearby_transaction(self, max_count, latlng, rules):
+        """Returns transaction data near 'latlng' sorted by 'rules'.
+
+        Args:
+          max_count: int.
+          latlng: tuple.
+          rules: Rules. None: no criteria entered so far.
+
+        Returns:
+          list of transaction data.
+        """
 
         db_files = [
             '/var/lib/twrealprice/twrealprice.db',
@@ -206,10 +217,10 @@ class TwRealPrice(object):
 
         nearby = []
         try:
-            lat_center, lng_center = LatLngIndex(coordinate[0], coordinate[1])
+            lat_center, lng_center = LatLngIndex(latlng[0], latlng[1])
         except ValueError:
             traceback.print_exc()
-            return [], []
+            return []
 
         for lat_in, lng_in in GenNearbyIndex(lat_center, lng_center, 1):
             for row in conn.execute(
@@ -220,41 +231,40 @@ class TwRealPrice(object):
 
         conn.close()
         end_time = time.time()
-        _LOG.info('DB search: %.6f sec ...', end_time - start_time)
+        _LOG.info('DB search (%d data): %.6f sec ...',
+                  len(nearby), end_time - start_time)
 
-        # Apply filter / sorting
-        query = Memory.Get('filter', '').encode('utf-8')
-        if query:
-            # User enters the criteria for sorting
-            for s in nearby:
-                s['AGE'] = Age(s['建築完成年月'])[:-3]  # remove 年
+        for s in nearby:
+            s['AGE'] = Age(s['建築完成年月'])[:-3]  # remove 年
 
-            rules = twrealprice_rule.Rules.Create(
-                latlng=(coordinate[0], coordinate[1])
-            )
-            rules.ParseQuery(query)
-            filters = rules.filters
-            rules.CountScore(nearby)
-            rules.Sort()
+        if not rules:
+            rules = twrealprice_rule.Rules()
+            rules.AddPureSortingRules(latlng)
+        rules.CountScore(nearby)
+        rules.Sort()
 
-            Memory.Set('cached_transaction', rules.Top(MAX_CACHE_TRANS))
-            return rules.Top(max_count), filters
-        else:
-            Memory.Set('cached_transaction', nearby[:MAX_CACHE_TRANS])
-            return nearby[:max_count], []
+        Memory.Set('cached_transaction', rules.Top(MAX_CACHE_TRANS))
+        return rules.Top(max_count)
 
 
-def run(content_config, env, variables):
-    """
-    content_config schema:
-    {
-       "api_key": "xxxxx",
-       "location": "location variables",
-       "max_count": 3
-       "distance_threshold": 0.050,
-    }
-    """
-    msgs = []
+class UserInput(object):
+    KEYS = ['reset', 'more_data', 'location', 'query']
+
+    def __init__(self):
+        # First time, read from bot and clear them.
+        self.singleton = {k: Memory.Get(k, None) for k in self.KEYS}
+        _LOG.info('User input: %r', self.singleton)
+
+        for k in self.KEYS:
+            Memory.Set(k, None)
+
+    def Get(self, key):
+        return self.singleton[key]
+
+
+def run(unused_content_config, env, unused_variables):
+    user_input = UserInput()
+    msgs = []  # The output messages.
 
     # output limit and map size
     size = (500, 260)
@@ -262,79 +272,154 @@ def run(content_config, env, variables):
         max_count = 2
         max_text_len = 60
     else:
-        max_count = content_config.get('max_count', 10)
+        max_count = 5
         max_text_len = Message.MAX_TEXT_LEN
 
-    # Check if the user requests more data. Maintain the pointers.
-    more_data = Memory.Get('more_data', None)
-    next_data_index = Memory.Get('next_data_index', max_count)
+    address = u''   # str. The address user entered.
+
+    geocoder = GoogleMapsGeocodingAPI(api_key=GOOGLE_STATIC_MAP_API_KEY)
+
+    more_data = user_input.Get('more_data')
+    latlng = Memory.Get('latlng', None)  # tuple. User's current location.
     cached_transaction = Memory.Get('cached_transaction', None)
-    Memory.Set('more_data', '0')
-    Memory.Set('next_data_index', max_count)  # could be updated soon below
-    if more_data == '1' and cached_transaction:
+    next_data_index = Memory.Get('next_data_index', max_count)
+    if more_data:
         # Yes, user clicks 'more data', dump data from cache.
 
-        if env['platform_type'] == SupportedPlatform.Line:
-            max_count = 4
+        if not cached_transaction:
+            return [Message(u'請重新輸入地址或條件。')]
 
         trans = cached_transaction[next_data_index:next_data_index + max_count]
         Memory.Set('next_data_index', next_data_index + max_count)
 
         if not trans:
-            return [Message('沒有更多物件了，請重新輸入地址或條件。')]
+            return [Message(u'沒有更多物件了，請重新輸入地址或條件。')]
 
     else:
-        # No. User enters an address or a query.
-        location = Resolve(content_config['location'], variables)
-        if 'coordinates' not in location:
-            location = Memory.Get('location')
+        # Nope. User is not entering 'more data'.
 
-            if 'coordinates' not in location:
-                return [Message('不正確的輸入，請重新輸入')]
+        reset = user_input.Get('reset')
+        location = user_input.Get('location')
 
-        c = (location['coordinates']['lat'], location['coordinates']['long'])
+        if reset:
+            latlng = Memory.Get('latlng', None)
+            rules = None
+
+        # User uploaded current location.
+        elif location and 'coordinates' in location:
+            latlng = (
+                location['coordinates']['lat'],
+                location['coordinates']['long'])
+
+            rules = Deser(Memory.Get('rules', None))
+
+        else:  # User entered either an address or criteria; or both.
+            query = user_input.Get('query')
+            rules = twrealprice_rule.Rules.Create()
+            query = rules.ParseQuery(query)
+
+            address = query.strip()
+            _LOG.info(
+                'User query: address=[%s] filters:[%s]',
+                address, rules.filters)
+            if address:
+                if latlng:
+                    center = {'lat': latlng[0], 'long': latlng[1]}
+                else:
+                    center = None
+
+                geo_results = geocoder.query_top_n(
+                    n=3,
+                    address=address,
+                    language='zh_TW',
+                    region='TW',
+                    bounds=[[
+                        [20.0, 118.0],
+                        [26.0, 123.0]]],
+                    center=center)
+
+                if not geo_results and not rules.filters:
+                    return [Message(u'我不認識這個地址: [%s]' % address)]
+
+                if len(geo_results) == 1:
+                    latlng = (geo_results[0]['location'][0],
+                              geo_results[0]['location'][1])
+
+                if len(geo_results) > 1:
+                    m = Message(buttons_text=u'你指的是以下哪一個地址呢?')
+                    for r in geo_results:
+                        m.add_button(Message.Button(
+                            Message.ButtonType.POSTBACK,
+                            r['address'],
+                            payload=LocationPayload(
+                                r['location'],
+                                False)))
+
+                    # If user also entered criteria, save it for later query.
+                    if rules.filters:
+                        rules.AddPureSortingRules(latlng)
+                        Memory.Set('rules', cPickle.dumps(rules))
+
+                    return [m]
+
+            if not rules.filters:
+                # If user didn't enter filter, use the previous one.
+                rules = Deser(Memory.Get('rules', None))
+                if rules:
+                    _LOG.info('User prev rules: %s', rules.filters)
+            if rules:
+                rules.AddPureSortingRules(latlng)
+
+        next_data_index = max_count
+
+        if not latlng:
+            return [Message(
+                u'我還不知道你想查的地方是哪裡。請輸入地址或是送出你的位置。')]
+
+        Memory.Set('rules', cPickle.dumps(rules))
+        Memory.Set('latlng', latlng)
+        Memory.Set('next_data_index', next_data_index)
+
         twrealprice = TwRealPrice()
-
         try:
-            trans, filters = twrealprice.nearby_transaction(
-                max_count, c, content_config['distance_threshold'])
+            trans = twrealprice.nearby_transaction(max_count, latlng, rules)
         except IOError:
-            return [Message('資料庫找不到，趕快回報給粉絲頁管理員，謝謝。')]
+            return [Message(u'資料庫找不到，趕快回報給粉絲頁管理員，謝謝。')]
 
-        if Memory.Get('filter'):
-            if filters:
-                msgs.append(Message('轉譯後的篩選條件: %s' % ' '.join(filters)))
-            else:
-                msgs.append(Message(
-                    '您輸入的條件我看不懂，請試試別的條件，或是「重設條件」。'))
-                return msgs
+        if rules and rules.filters:
+            msgs.append(Message(u'篩選條件: %s' % ' '.join(rules.filters)))
 
         if not trans:
-            msg = Message('對不起，這裡附近沒有成交行情喔！')
+            msg = Message(u'對不起，找不到成交行情喔！試試別的地址或條件。')
             msgs.append(msg)
             return msgs
 
-        m = GoogleStaticMapAPIRequestBuilder(content_config['api_key'], size)
-        m.add_marker(c, 'purple')
-        for s in trans:
-            lat = float(s['latlng'][0])
-            lng = float(s['latlng'][1])
-            m.add_marker((lat, lng))
+    # Construct main result cards.
+    main_map = GoogleStaticMapAPIRequestBuilder(
+        GOOGLE_STATIC_MAP_API_KEY, size)
+    style = MarkerStyle()
+    main_map.add_marker(latlng, style.next())
+    for s in trans:
+        lat = float(s['latlng'][0])
+        lng = float(s['latlng'][1])
+        main_map.add_marker((lat, lng), style.next())
 
-        subtitle = '搜尋結果僅供參考，詳細完整實價登錄資料，以內政部公佈為準。'
+    subtitle = u'搜尋結果僅供參考，詳細完整實價登錄資料，以內政部公佈為準。'
 
-        # Construct main result cards.
-        msg = Message()
-        b = Message.Bubble('附近的成交行情',
-                           image_url=m.build_url(),
-                           subtitle=subtitle)
+    msg = Message()
+    b = Message.Bubble(u'%s附近的成交行情' % address,
+                       image_url=main_map.build_url(),
+                       subtitle=subtitle)
 
-        msg.add_bubble(b)
-        msgs.append(msg)
+    msg.add_bubble(b)
+    msgs.append(msg)
 
     # At this point, 'trans' must have data to show.
 
+    i = -1
     for s in trans:
+        # In this loop, the strings are concatenated in utf-8 first because the
+        # transaction data come with utf-8 encoded.
         try:
             def UnitPrice(s):
                 try:
@@ -378,8 +463,12 @@ def run(content_config, env, variables):
                 '備註: ' + s['備註'] if s['備註'] else '',
             ]).decode('utf-8')[:max_text_len]
 
+            i += 1
+
             msg = Message()
-            msg.add_bubble(Message.Bubble(title, subtitle=subtitle))
+            msg.add_bubble(Message.Bubble(
+                (u'%c: ' % (i + ord('A'))) + title,
+                subtitle=subtitle))
             msgs.append(msg)
 
         except ValueError:
--- bb8/backend/content_modules/twrealprice_rule_unittest.py
+++ bb8/backend/content_modules/twrealprice_rule_unittest.py
@@ -11,12 +11,12 @@ import twrealprice_rule
 class TestRule(unittest.TestCase):
     def setUp(self):
         self.rule = twrealprice_rule.Rule(
-            parse_query=lambda query: '套房' in query,
+            parse_query=lambda query: u'套房' in query,
             count_score=lambda tran: (
                 1.0 if '套房' in tran['建物型態'] else -1.0))
 
     def test_parse(self):
-        self.assertEqual(bool(self.rule.ParseQuery('XX套房XX')), True)
+        self.assertEqual(bool(self.rule.ParseQuery(u'XX套房XX')), True)
 
     def test_score(self):
         self.assertGreater(self.rule.CountScore(
@@ -28,14 +28,14 @@ class TestRule(unittest.TestCase):
 class TestMatcher(unittest.TestCase):
     def setUp(self):
         self.matcher = twrealprice_rule.Matcher(
-            accept_words=['透天', '整棟'],
+            accept_words=[u'透天', u'整棟'],
             tran_key='建物型態',
             tran_value='透天厝',
             weight=1000)
 
     def test_parser(self):
-        self.assertTrue(self.matcher.ParseQuery(query='我想買透天厝'))
-        self.assertTrue(self.matcher.ParseQuery(query='我想買整棟的'))
+        self.assertTrue(self.matcher.ParseQuery(query=u'我想買透天厝'))
+        self.assertTrue(self.matcher.ParseQuery(query=u'我想買整棟的'))
 
     def test_score(self):
         self.assertGreater(self.matcher.CountScore({'建物型態': '透天厝'}), 0)
@@ -44,23 +44,34 @@ class TestMatcher(unittest.TestCase):
 class TestNumber(unittest.TestCase):
     def setUp(self):
         self.integer = twrealprice_rule.Number(
-            unit='樓',
+            unit=u'樓',
             tran_key='移轉層次',
             weight=10)
         self.float = twrealprice_rule.Number(
-            unit='坪',
+            unit=u'坪',
             tran_key='建物移轉總面積平方公尺',
             weight=10,
             scale=3.3)
+        self.room = twrealprice_rule.Number(
+            unit=u'房',
+            tran_key='建物現況格局-房',
+            weight=10)  # To regress slope=1.0 and scale=1.0
 
     def test_integer(self):
-        self.assertTrue(self.integer.ParseQuery(query='我想買5樓'))
+        self.assertTrue(self.integer.ParseQuery(query=u'我想買5樓'))
         self.assertEqual(self.integer.CountScore({'移轉層次': '五層'}), 1.0)
 
     def test_ping(self):
-        self.assertTrue(self.float.ParseQuery(query='室內19.23坪'))
+        self.assertTrue(self.float.ParseQuery(query=u'室內19.23坪'))
         self.assertAlmostEqual(self.float.CountScore(
             {'建物移轉總面積平方公尺': '63.459'}), 1.0)
+
+    def test_room(self):
+        self.assertTrue(self.room.ParseQuery(query=u'2房'))
+        self.assertAlmostEqual(self.room.CountScore(
+            {'建物現況格局-房': '2'}), 1.0)
+        self.assertAlmostEqual(self.room.CountScore(
+            {'建物現況格局-房': '3'}), 0.0)
 
 
 class TestDays(unittest.TestCase):
@@ -86,7 +97,21 @@ class TestLocation(unittest.TestCase):
         self.assertEqual(self.location.CountScore(
             {'latlng': (25.0554948, 121.6004863)}), 1.0)
         self.assertAlmostEqual(self.location.CountScore(
-            {'latlng': (25.0557478, 121.6060973)}), 0.43832990109835046)
+            {'latlng': (25.0557478, 121.6030973)}), 0.47535421473121797)
+
+
+class TestPrice(unittest.TestCase):
+    def setUp(self):
+        self.price = twrealprice_rule.Number(
+            weight=10.0, unit=u'萬', tran_key='總價元', slope=-0.2, scale=10000)
+
+    def test_price(self):
+        self.assertTrue(self.price.ParseQuery(query=u'1000.0萬'))
+        self.assertEqual(self.price.CountScore({'總價元': '10000000'}), 1.0)
+        self.assertTrue(self.price.ParseQuery(query=u'100萬'))
+        self.assertEqual(self.price.CountScore({'總價元': '900000'}), 0.5)
+        self.assertTrue(self.price.ParseQuery(query=u'10萬'))
+        self.assertEqual(self.price.CountScore({'總價元': '110000'}), 0.5)
 
 
 class TestRules(unittest.TestCase):
@@ -94,11 +119,11 @@ class TestRules(unittest.TestCase):
         self.rules = twrealprice_rule.Rules()
         self.rules.Add(twrealprice_rule.Matcher(
             weight=1000,
-            accept_words=['透天', '獨棟'], tran_key='建物型態',
+            accept_words=[u'透天', u'獨棟'], tran_key='建物型態',
             tran_value='透天'))
 
     def test_single(self):
-        self.rules.ParseQuery('爺們兒只買獨棟別墅')
+        self.rules.ParseQuery(u'爺們兒只買獨棟別墅')
         self.rules.CountScore([
             {'建物型態': '公寓'},
             {'建物型態': '透天厝'},
@@ -113,16 +138,24 @@ class TestCanonize(unittest.TestCase):
         self.canonizer = twrealprice_rule.Rules.Canonize
 
     def test_numbers(self):
-        self.assertEqual(self.canonizer('五十六'), '56')
-        self.assertEqual(self.canonizer('五十'), '50')
-        self.assertEqual(self.canonizer('十六'), '16')
-        self.assertEqual(self.canonizer('一十六'), '16')
-        self.assertEqual(self.canonizer('十'), '10')
-        self.assertEqual(self.canonizer('一千二百一十六'), '1216')
-        self.assertEqual(self.canonizer('九千零三十'), '9030')
-        self.assertEqual(self.canonizer('１９７８'), '1978')
-        self.assertEqual(self.canonizer('叁層'), '3樓')
-        self.assertEqual(self.canonizer('三十坪'), '30坪')
+        self.assertEqual(self.canonizer(u'五十六'), u'56')
+        self.assertEqual(self.canonizer(u'五十'), u'50')
+        self.assertEqual(self.canonizer(u'十六'), u'16')
+        self.assertEqual(self.canonizer(u'一十六'), u'16')
+        self.assertEqual(self.canonizer(u'十'), u'10')
+        self.assertEqual(self.canonizer(u'一千二百一十六'), u'1216')
+        self.assertEqual(self.canonizer(u'九千零三十'), u'9030')
+        self.assertEqual(self.canonizer(u'１９７８'), u'1978')
+        self.assertEqual(self.canonizer(u'叁層'), u'3樓')
+        self.assertEqual(self.canonizer(u'三十坪'), u'30坪')
+
+        self.assertEqual(self.canonizer(u'一千五百萬'), u'1500萬')
+        self.assertEqual(self.canonizer(u'一千五百六十八萬'), u'1568萬')
+        self.assertEqual(self.canonizer(u'二億'), u'20000萬')
+        self.assertEqual(self.canonizer(u'二億五千一百萬'), u'25100萬')
+        self.assertEqual(self.canonizer(
+            u'二十三億五千一百六十八萬'), u'235168萬')
+
 
 if __name__ == "__main__":
     unittest.main()
