0f39fd6ed6ffa7f1f2d2486c3fac785ead03a121
Kevin Li <kevin@compose.ai>
2016-10-17 16:18:29 +0000

    frontend: combine react-router-redux with redux-immutable

--- bb8/frontend/reducers/index.js
+++ bb8/frontend/reducers/index.js
@@ -1,10 +1,26 @@
+import Immutable from 'immutable'
 import { combineReducers } from 'redux-immutable'
+import { LOCATION_CHANGE } from 'react-router-redux'
 import AccountsReducer from './AccountsReducer'
 import BotsReducer from './BotsReducer'
+
+const initialState = Immutable.fromJS({
+  locationBeforeTransitions: null,
+})
+
+function routingReducer(state = initialState, action) {
+  if (action.type === LOCATION_CHANGE) {
+    return state.merge({
+      locationBeforeTransitions: action.payload,
+    })
+  }
+  return state
+}
 
 const rootReducer = combineReducers({
   bots: BotsReducer,
   account: AccountsReducer,
+  routing: routingReducer,
 })
 
 export default rootReducer
