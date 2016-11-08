8266a1d458609bc405e8d67a5feb1e732e8a992c
LAI <salas0316@gmail.com>
2016-11-08 20:44:13 +0800

    frontend: layout adjustments
    Remove menu iconMenu, move bot picker to left, remove user setting menu,
    adjust layouts.

--- bb8/frontend/components/TopBar/RightToolBarGroup.jsx
+++ bb8/frontend/components/TopBar/RightToolBarGroup.jsx
@@ -1,126 +1,44 @@
 import React from 'react'
-import { connect } from 'react-redux'
 
-import ContentAdd from 'material-ui/svg-icons/content/add'
-import IconButton from 'material-ui/IconButton'
-import IconMenu from 'material-ui/IconMenu'
-import Menu from 'material-ui/Menu'
-import MenuItem from 'material-ui/MenuItem'
-import NavigationExpandMoreIcon from 'material-ui/svg-icons/navigation/expand-more'
-import Popover from 'material-ui/Popover'
-import RaisedButton from 'material-ui/RaisedButton'
+import IconMoreVert from 'material-ui/svg-icons/navigation/more-vert'
 import { ToolbarGroup } from 'material-ui/Toolbar'
+import FlatButton from 'material-ui/FlatButton'
 
-import { setActiveBot } from '../../actions'
+const styles = {
+  container: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+  button: {
+    height: '100%',
+    color: 'white',
+  },
+}
 
 class RightToolbarGroup extends React.Component {
 
   constructor(props) {
     super(props)
 
-    this.handleTouchTap = this.handleTouchTap.bind(this)
-    this.handleRequestClose = this.handleRequestClose.bind(this)
+
+    // TODO: popover
     this.state = {
       open: false,
     }
   }
 
-  handleTouchTap(event) {
-    // This prevents ghost click.
-    event.preventDefault()
-
-    if (this.props.ids.length) {
-      this.setState({
-        open: true,
-        anchorEl: event.currentTarget,
-      })
-    }
-    // else {
-    //   TODO: open create bot dialog
-    // }
-  }
-
-  handleRequestClose() {
-    this.setState({
-      open: false,
-    })
-  }
-
   render() {
-    const { ids, activeId, onSetActiveBot } = this.props
-    const length = ids.length
     return (
-      <ToolbarGroup>
-        <RaisedButton
-          secondary
-          onTouchTap={this.handleTouchTap}
-          label={activeId === -1 ? 'Add a bot' : this.props.data[activeId].name}
+      <ToolbarGroup style={styles.container}>
+        <FlatButton
+          icon={<IconMoreVert />}
+          style={styles.button}
         />
-        <Popover
-          open={this.state.open}
-          anchorEl={this.state.anchorEl}
-          anchorOrigin={{ horizontal: 'left', vertical: 'bottom' }}
-          targetOrigin={{ horizontal: 'left', vertical: 'top' }}
-          onRequestClose={this.handleRequestClose}
-        >
-          <Menu>
-            {length === 0 ? null :
-               ids.map(id =>
-                 (<MenuItem
-                   key={id}
-                   primaryText={this.props.data[id].name}
-                   onTouchTap={() => onSetActiveBot(id)}
-                 />))
-            }
-            <MenuItem primaryText="Add a bot" leftIcon={<ContentAdd />} />
-          </Menu>
-        </Popover>
-        <IconMenu
-          iconButtonElement={
-            <IconButton
-              touch
-              style={{ width: '56px', height: '56px' }}
-              iconStyle={{ color: 'white' }}
-            >
-              <NavigationExpandMoreIcon />
-            </IconButton>
-          }
-          anchorOrigin={{ horizontal: 'left', vertical: 'bottom' }}
-          targetOrigin={{ horizontal: 'left', vertical: 'top' }}
-        >
-          <MenuItem primaryText="My Account" />
-        </IconMenu>
       </ToolbarGroup>)
   }
 }
 
 RightToolbarGroup.propTypes = {
-  onSetActiveBot: React.PropTypes.func,
-  ids: React.PropTypes.arrayOf(React.PropTypes.number),
-  data: React.PropTypes.objectOf(React.PropTypes.shape({
-    name: React.PropTypes.string,
-  })),
-  activeId: React.PropTypes.number,
 }
 
-const mapStateToProps = state => ({
-  data: state.bots.ids.reduce((obj, id) =>
-    Object.assign(obj, {
-      [id]: state.entities.bots[id],
-    }), {}),
-  ids: state.bots.ids,
-  activeId: state.bots.active,
-})
-
-const mapDispatchToProps = dispatch => ({
-  onSetActiveBot(id) {
-    dispatch(setActiveBot(id))
-  },
-})
-
-const ConnectedRightToolbarGroup = connect(
-  mapStateToProps,
-  mapDispatchToProps,
-)(RightToolbarGroup)
-
-export default ConnectedRightToolbarGroup
+export default RightToolbarGroup
--- bb8/frontend/components/TopBar/index.jsx
+++ bb8/frontend/components/TopBar/index.jsx
@@ -1,103 +1,40 @@
 import React from 'react'
-import { hashHistory } from 'react-router'
 
-import ActionDashboard from 'material-ui/svg-icons/action/dashboard'
-import ActionTimeline from 'material-ui/svg-icons/action/timeline'
-import ActionHelp from 'material-ui/svg-icons/action/help'
-import CommunicationChatBubbleOutline from 'material-ui/svg-icons/communication/chat-bubble-outline'
-import ImageLooks from 'material-ui/svg-icons/image/looks'
-import HardwareDeviceHub from 'material-ui/svg-icons/hardware/device-hub'
-import Drawer from 'material-ui/Drawer'
-import IconButton from 'material-ui/IconButton'
-import NavigationMenu from 'material-ui/svg-icons/navigation/menu'
-import { List, ListItem } from 'material-ui/List'
+import stylePropType from 'react-style-proptype'
 import { Toolbar, ToolbarGroup } from 'material-ui/Toolbar'
 
-import ConnectedRightToolbarGroup from './RightToolBarGroup'
-import LogoPNG from '../../assets/logo.png'
+import ConnectedBotPickerButton from './BotPickerButton'
+import RightToolbarGroup from './RightToolBarGroup'
+
+const styles = {
+  container: {
+    justifyContent: 'space-between',
+  },
+  buttonContainer: {
+    alignItems: 'center',
+  },
+}
 
 class TopBar extends React.Component {
 
   constructor(props) {
     super(props)
 
-    this.handleClose = this.handleClose.bind(this)
-    this.handleToggle = this.handleToggle.bind(this)
-    this.handleClose = this.handleClose.bind(this)
-    this.state = {
-      leftMenuOpen: false,
-    }
-  }
-
-  handleLink(path) {
-    return () => {
-      this.handleToggle()
-      hashHistory.push(path)
-    }
-  }
-
-  handleToggle() {
-    this.setState({ leftMenuOpen: !this.state.leftMenuOpen })
-  }
-
-  handleClose() {
-    this.setState({ leftMenuOpen: false })
+    this.state = {}
   }
 
   render() {
-    return (<Toolbar className="b-toolbar">
-      <ToolbarGroup firstChild>
-        <IconButton
-          onTouchTap={this.handleToggle}
-          style={{ width: '56px', height: '56px' }}
-          iconStyle={{ color: 'white' }}
-        >
-          <NavigationMenu />
-        </IconButton>
-        <Drawer
-          docked={false}
-          width={320}
-          open={this.state.leftMenuOpen}
-          onRequestChange={leftMenuOpen => this.setState({ leftMenuOpen })}
-        >
-          <img className="b-logo" src={LogoPNG} alt="logo" />
-          <List>
-            <ListItem
-              onTouchTap={this.handleLink('/dashboard')}
-              primaryText="Dashboard"
-              leftIcon={<ActionDashboard />}
-            />
-            <ListItem
-              onTouchTap={this.handleLink('/flow')}
-              primaryText="Flow"
-              leftIcon={<HardwareDeviceHub />}
-            />
-            <ListItem
-              onTouchTap={this.handleLink('/broadcast')}
-              primaryText="Broadcast"
-              leftIcon={<ImageLooks />}
-            />
-            <ListItem
-              onTouchTap={this.handleLink('/platforms')}
-              primaryText="Platforms"
-              leftIcon={<CommunicationChatBubbleOutline />}
-            />
-            <ListItem
-              onTouchTap={this.handleLink('/analytics')}
-              primaryText="Analytics"
-              leftIcon={<ActionTimeline />}
-            />
-            <ListItem
-              onTouchTap={this.handleLink('/help')}
-              primaryText="Help"
-              leftIcon={<ActionHelp />}
-            />
-          </List>
-        </Drawer>
+    return (<Toolbar style={{ ...styles.container, ...this.props.style }}>
+      <ToolbarGroup firstChild style={styles.buttonContainer}>
+        <ConnectedBotPickerButton />
       </ToolbarGroup>
-      <ConnectedRightToolbarGroup />
+      <RightToolbarGroup />
     </Toolbar>)
   }
+}
+
+TopBar.propTypes = {
+  style: stylePropType,
 }
 
 export default TopBar
