45e592c04e215a434525884e70c991ede1d878f4
Wei-Ning Huang <aitjcize@compose.ai>
2016-11-13 04:30:38 +0000

    apps: drama: add vmus popular drama crawler

--- apps/drama/drama/keywords.py
+++ apps/drama/drama/keywords.py
@@ -18,6 +18,7 @@ def extract_popular_dramas():
     """Extracts popular dramas from dramaq.biz, the order on their
         front page impliest the popularity order
     """
+    # Drama Q
     country_list = {
         'kr': 'http://www.dramaq.biz/',
         'tw': 'http://www.dramaq.biz/tw/',
@@ -25,7 +26,7 @@ def extract_popular_dramas():
         'jp': 'http://www.dramaq.biz/jp/',
     }
 
-    def popular_drama(country):
+    def popular_dramaq(country):
         try:
             url = country_list[country]
             with contextlib.closing(urllib.urlopen(url)) as page:
@@ -34,8 +35,25 @@ def extract_popular_dramas():
             logging.warning('Cannot fetch country: ' + country)
             return []
 
-        hrefs = response.xpath('//td/a[1]/@href')
+        hrefs = response.xpath('//td/a[1]/@href').extract()
         links = [unicode(urlparse.urljoin(url, href)) for href in hrefs]
         return links
 
-    return {country: popular_drama(country) for country in country_list}
+    # Vmus
+    def popular_vmus():
+        try:
+            url = 'http://vmus.co/?s=%E9%80%A3%E8%BC%89%E4%B8%AD'
+            with contextlib.closing(urllib.urlopen(url)) as page:
+                response = lxml.html.fromstring(page.read())
+        except Exception:
+            logging.warning('Cannot fetch popular drama for vmus')
+            return []
+
+        return response.xpath('//html/body/div[@id="wrapper"]/div[@id="wrap"]'
+                              '/section[@id="content"]/article'
+                              '/h2/a/@href').extract()
+
+    result = {country: popular_dramaq(country) for country in country_list}
+    result['us'] = popular_vmus()
+
+    return result
