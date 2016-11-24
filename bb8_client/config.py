15f5ead93e679cf5d99ab99d9d3064484499ffa5
Wei-Ning Huang <aitjcize@compose.ai>
2016-11-24 11:28:42 +0000

    bb8: switch resource domain to a.compose.ai

--- bb8_client/config.py
+++ bb8_client/config.py
@@ -27,5 +27,5 @@ class DevelopmentConfig(Config):
 
 class DeployConfig(Config):
     HOSTNAME = 'bot.compose.ai'
-    RESOURCE_HOSTNAME = 'i.compose.ai'
+    RESOURCE_HOSTNAME = 'a.compose.ai'
     HTTP_PORT = 5000
