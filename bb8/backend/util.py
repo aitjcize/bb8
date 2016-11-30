19709471eb9d45f4eedda94c5785a0fad1b148f1
Wei-Ning Huang <aitjcize@compose.ai>
2016-11-30 05:37:47 +0000

    util: fix base64 encode error

--- bb8/backend/util.py
+++ bb8/backend/util.py
@@ -12,6 +12,8 @@ from bb8 import config
 
 
 def image_convert_url(url, max_size_tuple):
+    if isinstance(url, unicode):
+        url = url.encode('utf8')
     return ('https://%s:%d/api/util/image_convert?size=%dx%d&url=%s' %
             ((config.HOSTNAME, config.HTTP_PORT) + max_size_tuple +
              (base64.b64encode(url),)))
