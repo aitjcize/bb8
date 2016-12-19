b9a52c60cfe77f9118ef2e7d1db35785334a6522
salas0316 <salas@gmail.com>
2016-12-19 12:13:04 +0000

    frontend: Modified loginForm styles.

--- bb8/frontend/modules/Login.jsx
+++ bb8/frontend/modules/Login.jsx
@@ -1,27 +1,69 @@
 import React from 'react'
 import { Link } from 'react-router'
 
-import { Card } from 'material-ui/Card'
+import Paper from 'material-ui/Paper'
 
 import LoginForm from '../components/forms/LoginForm'
 import FacebookAuth from '../components/FacebookAuth'
 import Notification from '../components/Notification'
-import LogoPNG from '../assets/logo.png'
+
+const styles = {
+  container: {
+    height: '100vh',
+    overflow: 'hidden',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  loginBox: {
+    width: '25vw',
+    minWidth: '30em',
+    position: 'relative',
+  },
+  rows: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    margin: '1em 4vw',
+  },
+  loginForm: {
+    alignSelf: 'stretch',
+  },
+  facebookAuthButton: {
+    margin: '1.5em 0',
+  },
+  link: {
+    color: '#1DE9B6',
+    fontWeight: 'bold',
+    padding: '0 .5em',
+  },
+  signupHint: {
+    position: 'absolute',
+    width: '100%',
+    textAlign: 'center',
+    fontSize: '.875em',
+    margin: '.5em 0',
+  },
+}
 
 const Login = () => (
-  <Card className="b-login-card">
-    <Notification />
-    <div className="b-login-card__container">
-      <span className="b-login-card__title"> Login </span>
-      <img className="b-login-card__logo" src={LogoPNG} alt="logo" />
-      <LoginForm />
-      <FacebookAuth />
-      <div className="b-login-card__bottom">
-        Do not have account yet? go to
-        <Link to="/signup" > Signup </Link>
+  <div style={styles.container}>
+    <Paper style={styles.loginBox}>
+      <Notification />
+      <div style={styles.rows}>
+        <LoginForm style={styles.loginForm} />
       </div>
-    </div>
-  </Card>
+      <div style={styles.rows}>
+        or
+        <FacebookAuth style={styles.facebookAuthButton} />
+      </div>
+      <div style={styles.signupHint}>
+        Do not have account yet?
+        <Link to="/signup" style={styles.link} > Signup now </Link>
+      </div>
+    </Paper>
+  </div>
 )
 
 export default Login
