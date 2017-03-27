aitjcize/bb8

commit b4e66459bef235de5465423b2bc6d0597e89f3c9

platform: add button to refresh page info
Fix #238

bb8/frontend/components/Platform/PlatformCard.jsx
@@ -171,9 +171,10 @@ class PlatformCard extends React.Component {
       </CardHeader>
       {!selectedBotId && <CardActions>
         <FlatButton
-          label="Edit"
+          label="Refresh"
           onTouchTap={() =>
-            this.dialogActions.openUpdatePlatform(this.props.platform)}
+            this.platformActions.refreshPage(this.props.platform)
+          }
         />
         <FlatButton
           label="Delete"

bb8/frontend/actions/platformActionCreators.js
@@ -17,3 +17,8 @@ export const updatePlatform = (platformId, platform) => ({
     platform,
   },
 })
+
+export const refreshPage = platform => ({
+  type: types.PLATFORMS_REFRESH.REQUEST,
+  payload: platform,
+})

bb8/frontend/api/facebook.js
@@ -100,3 +100,21 @@ export const refreshPage = () => authorize()
     },
     { access_token: response.accessToken })
   }))
+
+// refresh the name and access token of a given page
+export const getRefreshedPage = providerIdent => authorize()
+  .then(response => new Promise((resolve, reject) => {
+    // eslint-disable-next-line no-undef
+    FB.api('/me/accounts', (resp) => {
+      for (const page of resp.data) {
+        if (page.id === providerIdent) {
+          resolve(Object.assign({}, {
+            name: page.name,
+            accessToken: page.access_token,
+          }))
+        }
+      }
+      reject({ error: new Error(`Cannot find facebook page id: ${providerIdent}`) })
+    },
+    { access_token: response.accessToken })
+  }))

bb8/frontend/constants/ActionTypes.js
@@ -44,6 +44,7 @@ export default {
   PLATFORMS_CREATE: createRequestTypes('PLATFORMS_CREATE'),
   PLATFORMS_UPDATE: createRequestTypes('PLATFORMS_UPDATE'),
   PLATFORMS_DELETE: createRequestTypes('PLATFORMS_DELETE'),
+  PLATFORMS_REFRESH: createRequestTypes('PLATFORMS_REFRESH'),
 
   // UI
   NOTIFICATION_OPEN: 'NOTIFICATION_OPEN',

bb8/frontend/sagas/index.js
@@ -241,6 +241,44 @@ export function* getBotDefRevision() {
 
 /* Platform Sagas */
 
+export function* refreshPageSaga() {
+  while (true) {
+    const { payload } = yield take(types.PLATFORMS_REFRESH.REQUEST)
+    const platform = payload
+
+    const page = yield call(fbAPI.getRefreshedPage, platform.providerIdent)
+    const { response, error } = yield call(
+      api.updatePlatform,
+      platform.id,
+      Object.assign(
+        {}, platform,
+        {
+          name: page.name,
+          config: { accessToken: page.accessToken },
+        }))
+
+    if (error) {
+      yield put({
+        type: types.PLATFORMS_REFRESH.ERROR,
+        payload: error,
+      })
+      yield put({
+        type: types.NOTIFICATION_OPEN,
+        payload: 'Cannot refresh the page',
+      })
+    } else {
+      yield put({
+        type: types.PLATFORMS_REFRESH.SUCCESS,
+        payload: response,
+      })
+      yield put({
+        type: types.NOTIFICATION_OPEN,
+        payload: 'Successfully refreshed the page',
+      })
+    }
+  }
+}
+
 export function* fetchPlatformsSaga() {
   while (true) {
     yield take(types.PLATFORMS_LIST.REQUEST)
@@ -555,6 +593,7 @@ export default function* root() {
   yield fork(updateBotDefRevisionsSaga)
 
   /* Platform Saga */
+  yield fork(refreshPageSaga)
   yield fork(fetchPlatformsSaga)
   yield fork(createPlatformSaga)
   yield fork(updatePlatformSaga)

commit b02b98995899ce100122c2825c9a50552a1c9275

platform: fix line edit button bug introduced in b4e6645
Fix #238

bb8/frontend/components/Platform/PlatformCard.jsx
@@ -169,12 +169,17 @@ class PlatformCard extends React.Component {
       >
         {cardHeaderRightGroup}
       </CardHeader>
-      {!selectedBotId && <CardActions>
+      {!selectedBotId &&
+      <CardActions>
         <FlatButton
-          label="Refresh"
-          onTouchTap={() =>
-            this.platformActions.refreshPage(this.props.platform)
-          }
+          label={platform.typeEnum === 'Facebook' ? 'Refresh' : 'Edit'}
+          onTouchTap={() => {
+            if (platform.typeEnum === 'Facebook') {
+              this.platformActions.refreshPage(this.props.platform)
+            } else {
+              this.dialogActions.openUpdatePlatform(this.props.platform)
+            }
+          }}
         />
         <FlatButton
           label="Delete"